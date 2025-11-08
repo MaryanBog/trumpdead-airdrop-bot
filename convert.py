4686f8fb1401f3a82ecea049c102eb7833ba9ac1
MaryanBog <96717999+MaryanBog@users.noreply.github.com>
2025-11-08 18:26:19 +0300

    Delete convert.py
    delete

--- convert.py
+++
@@ -1,7 +0,0 @@
-from solana.keypair import Keypair
-import json
-
-base58_key = "2a3849HGXRiLLkDL8jzUk8WFhgLuortjZuuJK1XXPMohbAQ18gDTC8YYqLgY8T5YcyTahutKdxzeRgytNM895FAN"
-keypair = Keypair.from_base58_string(base58_key)
-secret_array = list(keypair.secret_key)
-print(json.dumps(secret_array))
