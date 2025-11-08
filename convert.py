24552e70ffd3ec2913323639055c76d9288ef24c
MaryanBog <96717999+MaryanBog@users.noreply.github.com>
2025-11-08 18:22:43 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 from solana.keypair import Keypair
 import json
 
-keypair = Keypair.from_base58_string("2a3849HGXRiLLkDL8jzUk8WFhgLuortjZuuJK1XXPMohbAQ18gDTC8YYqLgY8T5YcyTahutKdxzeRgytNM895FAN")
-secret = list(keypair.secret_key)
-print(json.dumps(secret))
+base58_key = "2a3849HGXRiLLkDL8jzUk8WFhgLuortjZuuJK1XXPMohbAQ18gDTC8YYqLgY8T5YcyTahutKdxzeRgytNM895FAN"
+keypair = Keypair.from_base58_string(base58_key)
+secret_array = list(keypair.secret_key)
+print(json.dumps(secret_array))
